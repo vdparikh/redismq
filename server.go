cdae38f85fcf4eba70bdd0366130b6e24014939f
Vishal Parikh <vdparikh@gmail.com>
2019-09-08 17:21:46 -0400

    Update server.go

--- server.go
+++ server.go
@@ -14,7 +14,7 @@ type Server struct {
 
 // NewServer returns a Server that can be started with Start()
 func NewServer(redisHost, redisPort, redisPassword string, redisDb int, port string) *Server {
-	observer := NewObserver(redisHost, redisPort, redisPassword, redisDb)
+	observer := NewObserver(redisHost, redisPort, redisPassword, redisDb, false)
 	s := &Server{
 		port:     port,
 		observer: observer,
